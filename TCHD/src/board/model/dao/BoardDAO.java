670bb39dc75debbf249517e0b29a2f95389a2270
ssd256 <63248384+ssd256@users.noreply.github.com>
2020-08-25 14:00:26 +0900

    8/25 13:59 김미경 BoardDAO 생성자 추가, board 쿼리파일 추가

--- TCHD/src/board/model/dao/BoardDAO.java
+++ TCHD/src/board/model/dao/BoardDAO.java
@@ -1,5 +1,29 @@
 package board.model.dao;
 
-public class BoardDAO {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
-}
+public class BoardDAO {
+	private Properties prop = new Properties();
+	
+	public BoardDAO() {
+		String fileName = BoardDAO.class.getResource("/sql/board/board-query.properties").getPath();
+		
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+} // class end
